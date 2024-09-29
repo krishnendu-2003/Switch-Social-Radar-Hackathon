f52b3f4d520faab75398bf23ec622f7013e06d4d
krishnendu-2003 <skrishnendu115@gmail.com>
2024-09-30 01:32:48 +0530

    Updated

--- client/SwitchSocial/src/screens/Profile.tsx
+++ client/SwitchSocial/src/screens/Profile.tsx
@@ -1,4 +1,4 @@
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Image,TouchableOpacity,  TextInput, ScrollView,  KeyboardAvoidingView  } from 'react-native';
 import React from 'react';
 import '../screens/NavigationBar'
 import { NavigationBar } from '../screens/NavigationBar';
@@ -6,7 +6,13 @@ import { NavigationBar } from '../screens/NavigationBar';
 export function Profile() {
   return (
     <View style={styles.container}>
+        <ImageBackground
+        source={require('../assets/MyProfile.png')}
+        style={styles.background}
+        resizeMode="cover">
       <NavigationBar/>
+
+        </ImageBackground>
     </View>
   );
 }
@@ -14,6 +20,10 @@ export function Profile() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  background: {
+    flex: 1, 
+    // alignItems: 'center',
   },
  
 });
