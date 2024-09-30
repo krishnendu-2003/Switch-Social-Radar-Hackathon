392f2c9aea62a2b4bc17bcc758d90b0dac5afd1b
krishnendu-2003 <skrishnendu115@gmail.com>
2024-09-30 12:24:09 +0530

    set navigation

--- client/SwitchSocial/src/screens/Profile.tsx
+++ client/SwitchSocial/src/screens/Profile.tsx
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet, ImageBackground, Image, ScrollView } from 'react-native';
 import React from 'react';
 import { NavigationBar } from '../screens/NavigationBar';
+import { ConnectButton } from '../components/sign-in/sign-in-ui';
 
 export function Profile() {
   return (
@@ -15,6 +16,35 @@ export function Profile() {
           <Text style={styles.profileTitle}>My Profile</Text>
           <Image source={require('../assets/ProfilePhoto.jpeg')} style={styles.profilePhoto} />
           <Text style={styles.username}>Echo_Whisper._</Text>
+
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View style={styles.connect}>
+            <Text style={styles.connectText}>120</Text>
+            <Text style={styles.connectTextDD}>Asset</Text>
+            </View>
+            <View style={styles.connect}>
+              <Text style={styles.connectText}>10K</Text>
+              <Text style={styles.connectTextDD}>Followers</Text>
+            </View>
+            <View style={styles.connect}>
+              <Text style={styles.connectText}>70K</Text>
+              <Text style={styles.connectTextDD}>Likes</Text>
+            </View>
+            <View style={styles.connect}>
+              <Text style={styles.connectText}>60</Text>
+              <Text style={styles.connectTextDD}>Bidding</Text>
+            </View>
+          </View>
+          <View>
+            <Text style={{color:'white'}}>
+              Your Balance
+            </Text>
+            <View  style={styles.connectText }>
+              <View><Image source={require('../assets/Ethicon.png')}/></View>
+              <View><Text style={{color:'white'}}>4,668 ETH</Text></View>
+              <View><Text style={{color:'white'}}>Add  > </Text></View>
+            </View>
+          </View>
 
         </ScrollView>
         <NavigationBar />
@@ -50,5 +80,29 @@ const styles = StyleSheet.create({
   username: {
     color: 'white',
     fontSize: 20,
+    marginBottom: 30
+
   },
+  connect: {
+    alignItems: 'center',
+    // justifyContent: 'center',
+    margin: 10,
+  },
+  connectText: {
+    borderWidth: 2,
+    borderColor: 'black',
+    backgroundColor: '#060925',
+    padding: 10,
+    borderRadius: 8,             
+    fontSize: 18, 
+    color: 'white'
+
+  },
+  connectTextDD: {
+     
+    padding: 10,
+    borderRadius: 8,             
+    fontSize: 15, 
+    color: 'white'
+  }
 });
